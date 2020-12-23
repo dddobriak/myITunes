7b5fffe6c04938733b64522e7350f8e38629823d
Pavel T <ironerg@gmail.com>
2020-12-23 11:43:55 +0200

    added videoPlayer

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,5 +1,12 @@
 export const radioPlayerInit = () => {
-  console.log('radioPlayerInit');
+  const radio = document.querySelector('.radio');
+  const radioCoverImg = document.querySelector('.radio-cover__img');
+  const radioHeader = document.querySelector('.radio-header');
+  const radioNavigation = document.querySelector('.radio-navigation');
+  const radioItem = document.querySelectorAll('.radio-item');
+  const radioStop = document.querySelector('.radio-stop');
+
+  const audio = new Audio();
 }
 
 radioPlayerInit();
